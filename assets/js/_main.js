cf2541c9c818ad259eb30cc7d1407ac58a9407d3
Daniel Julius Lasiman <dj.lasiman@gmail.com>
2018-08-11 20:20:39 +0700

    Disable smooth scroll

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -61,9 +61,6 @@ $(document).ready(function() {
     }, 400);
   });
 
-  // init smooth scroll
-  $("a").smoothScroll({ offset: -20 });
-
   // add lightbox class to all image links
   $(
     "a[href$='.jpg'],a[href$='.jpeg'],a[href$='.JPG'],a[href$='.png'],a[href$='.gif']"
